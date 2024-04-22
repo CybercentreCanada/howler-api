f4114662089d4f4fac29a5341897a04063ab5509
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2024-04-22 13:09:12 -0400

    Fixes to build process

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -4,10 +4,10 @@
 version=$( (cd .. && poetry version --short))
 
 # Clean build dir
-(cd .. && python setup.py clean --all && rm -rf dist)
+(cd .. && rm -rf dist)
 
 # Build wheel
-(cd .. && python setup.py bdist_wheel)
+(cd .. && poetry build)
 
 # Build container
 (cd .. && docker build --build-arg version=$version --no-cache -f docker/Dockerfile -t cccs/howler-api:latest -t cccs/howler-api:$version .)
