fe4b96bf46ccc4b9a5e0b4e3c782826fcdac821c
Matthew Rafuse <84039190+cccs-mdr@users.noreply.github.com>
2024-06-03 07:18:27 -0400

    Update build_container.sh

--- docker/build_container.sh
+++ docker/build_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 # Get version
-version=$( (cd .. && python setup.py --version))
+version=$( (cd .. && poetry version --short))
 
 # Clean build dir
 (cd .. && python setup.py clean --all && rm -rf dist)
